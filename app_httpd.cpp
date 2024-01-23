cec2d8c116469aed3092dda9c23f971230ef83db
Alex 'AdUser' Z <ad_user@runbox.com>
2024-01-23 23:12:40 +1000

    * use snprintf() instead sprintf() and pointer ariphmetics

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -272,19 +272,14 @@ static esp_err_t cmd_handler(httpd_req_t *req)
 }
 
 static esp_err_t status_handler(httpd_req_t *req) {
-  static char json_response[1024];
+  static char json[1024] = "";
 
   sensor_t * s = esp_camera_sensor_get();
-  char * p = json_response;
-  *p++ = '{';
-
-  p += sprintf(p, "\"framesize\":%u,", s->status.framesize);
-  p += sprintf(p, "\"quality\":%u,", s->status.quality);
-  *p++ = '}';
-  *p++ = 0;
+  snprintf(json, sizeof(json), "{\"framesize\":%u,\"quality\":%u}",
+    s->status.framesize, s->status.quality);
   httpd_resp_set_type(req, "application/json");
   httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-  return httpd_resp_send(req, json_response, strlen(json_response));
+  return httpd_resp_send(req, json, strlen(json));
 }
 
 static esp_err_t index_handler(httpd_req_t *req){
